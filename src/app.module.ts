422abaf476c4fc5804d58a5659271915bd846fb9
as87053 <as87053@lamatechnology.com>
2021-11-28 23:58:51 -0300

    Adjusts app.module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,26 @@ import { UserModule } from './user/user.module';
 import { UserService } from './user/user.service';
 import { UserController } from './user/user.controller';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
+import { User } from './user/entities/user.entity';
+import { config } from 'dotenv';
+
+config();
 
 @Module({
   imports: [
-    UserModule
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [User],
+      synchronize: true
+    })
   ],
   controllers: [
     UserController
@@ -14,4 +30,6 @@ import { Module } from '@nestjs/common';
     UserService
   ]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(private connection: Connection) {}
+}
